38f2b85237faa0e1641d2ee0b62cc087fa601a49
vmerkle8331 <vmerkle@uccs.edu>
2023-04-26 19:58:20 +0000

    test

---
+++ spec/features/projects_feature_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+RSpec.feature "Projects", type: :feature do
+    context "Update project" do
+      let(:project) { Project.create(title: "Test title", description: "Test content") }
+      before(:each) do
+        visit edit_project_path(project)
+      end
+      scenario "should be successful" do
+        within("form") do
+          fill_in "Description", with: "New description content"
+        end
+        click_button "Update Project"
+        expect(page).to have_content("Project was successfully updated")
+      end
+      scenario "should be successful" do
+        within("form") do
+          fill_in "Title", with: "New Title"
+        end
+        click_button "Update Project"
+        expect(page).to have_content("Project was successfully updated")
+      end
+      scenario "should fail" do
+        within("form") do
+          fill_in "Description", with: ""
+        end
+        click_button "Update Project"
+        expect(page).to have_content("Description can't be blank")
+      end
+      scenario "should fail" do
+        within("form") do
+          fill_in "Title", with: ""
+        end
+        click_button "Update Project"
+        expect(page).to have_content("Title can't be blank")
+      end
+    end
+end
