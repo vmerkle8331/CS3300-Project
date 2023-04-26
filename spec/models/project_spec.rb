d32ed2d3a30b1179400f0f87a0a63ea0e4ab9e55
vmerkle8331 <vmerkle@uccs.edu>
2023-04-26 18:57:08 +0000

    test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,16 +2,24 @@ require "rails_helper"
 describe "Project Attribute Requirements on Create", :type => :model do
   context "validation tests" do
     it "ensures the title is present when creating project" do
-      project = Project.new(description: "Content of the description")
-      expect(project.valid?).to eq(false)
+        project = Project.new(description: "Content of the description")
+        expect(project.valid?).to eq(false)
     end
     it "should not be able to save project when title missing" do
-      project = Project.new(description: "Some description content goes here")
-      expect(project.save).to eq(false)
+        project = Project.new(description: "Some description content goes here")
+        expect(project.save).to eq(false)
     end
+    it "ensures the description is present when creating project" do
+        project = Project.new(title: "Title")
+        expect(project.valid?).to eq(false)
+    end
+    it "should not be able to save project when description missing" do
+        project = Project.new(title: "Title")
+        expect(project.save).to eq(false)
+      end
     it "should be able to save project when have description and title" do
-      project = Project.new(title: "Title", description: "Content of the description")
-      expect(project.save).to eq(true)
+        project = Project.new(title: "Title", description: "Content of the description")
+        expect(project.save).to eq(true)
     end
   end
 end
@@ -23,6 +31,10 @@ describe "Project Attribute Requirements on Edit", :type => :model do
     it "ensures the title is present when editing project" do
       @project.update(:title => "New Title")
       expect(@project.title == "New Title")
+    end
+    it "ensures the description is present when editing project" do
+        @project.update(:description => "New description")
+        expect(@project.description == "New description")
     end
   end
 end
